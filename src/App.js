c0c31f38c7d101ae6aeca89de758f90b4332888e
MHH77 <mesfandiari77@gmail.com>
2025-01-31 14:29:22 +0330

    declaration first component

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
 import "./App.css";
 
 function App() {
-  var user = (
+  return (
+    <div>
+      <User />
+      <User />
+      <User />
+      <User />
+    </div>
+  );
+}
+
+const User = () => {
+  return (
     <div>
       <h1>Ali</h1>
       <h2>12</h2>
       <h3>ali@test.com</h3>
     </div>
   );
-  return (
-    <div>
-      {user}
-      {user}
-      {user}
-      {user}
-    </div>
-  );
-}
+};
 
 export default App;
