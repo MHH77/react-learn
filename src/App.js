3c5b2ecce1c33ec299a21e42cb82ff471d7515e4
MHH77 <mesfandiari77@gmail.com>
2025-02-07 14:19:00 +0330

    Using Ternary Operator

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import "./App.css";
 
 function App() {
+  let age = 10;
+
   return (
-    <div>
-      <User name={12} age="Programmer" email="oracle" />
-      <Job salary={12} position="Programmer" company="oracle" />
-    </div>
+    <div>{age > 20 ? <h1>age is over</h1> : <h1>age is not over</h1>}</div>
   );
 }
-
-const Job = (props) => {
-  return (
-    <div>
-      <h1>{props.position}</h1>
-      <h1>{props.company}</h1>
-      <h1>{props.salary}</h1>
-    </div>
-  );
-};
-
-const User = (props) => {
-  return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.age}</h2>
-      <h3>{props.email}</h3>
-    </div>
-  );
-};
-
 
 export default App;
