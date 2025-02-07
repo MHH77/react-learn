5576a49fbfec707f4efae5269e8a1102576f4601
MHH77 <mesfandiari77@gmail.com>
2025-02-07 16:13:42 +0330

    inline Conditional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
 
 function App() {
-  let age = 10;
+  let age = 20;
 
   return (
-    <div>{age > 20 ? <h1>age is over</h1> : <h1>age is not over</h1>}</div>
+    <div className="App">
+      {age > 20 ? <h1>age is over</h1> : <h1>age is not over</h1>}
+      {age === 20 && <h1>Age is 20 !</h1>}
+    </div>
   );
 }
 
